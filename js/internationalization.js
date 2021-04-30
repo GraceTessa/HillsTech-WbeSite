2289ed030d9f1d0b18fbf0dc40500203db956628
Tessa Grace <gracetessa98@gmail.com>
2021-04-30 11:31:23 +0100

    First commit

---
+++ js/internationalization.js
@@ -0,0 +1,246 @@
+
+let langdata = {
+        "En": {
+            "strings": {
+                "navMenu.home":"Home",
+                "navMenu.about": "Aboout",
+                "navMenu.services": "Services",
+                "navMenu.team": "Team",
+                "navMenu.showcase": "Showcase",
+                "navMenu.pricing": "Pricing",
+                "navMenu.testimonials": "Testimonials",
+                "navMenu.contact": "Contact",
+
+                "header.welcome": "We build helpful software",
+                "header.welcome.next": "For Business and Startups",
+                "header.manage_community": "Manage your community or group with ease!",   
+                "header.get_started": "Get Started",
+                "header.watch_video": "Watch Video",
+                "header.komunityoo": "Komunityoo",
+
+                "service.section.highly_configurable": "Highly configurable",
+                "service.section.customize": "Customize the use of the community by member according to your policy",
+                "service.section.report": "Detailed report",
+                "service.section.get_log": "Get a log of your community's activity at any time over a period of time",
+                "service.section.highly_performance": "High performance",
+                "service.section.every_operation": "Every operation is done quickly, accurately for the best user experience",
+                "service.section.connectivity": "Connectivity",
+                "service.section.stay_close" : "Stay close to your community, no matter where you are",
+
+                "about.section.about": "About",
+                "about.section.find": "Find Out More",
+                "about.section.about_us":"About Us",
+                "about.section.find_out": "Find out who we are, our goals and our vision for bringing people together",
+                "about.section.secure": "A secure web application that allows easy and simple management of an community or group",
+                "about.section.komunityoo": "Komunityoo is a secure web application that allows easy and simple management of an community or group.It is developed using recent technologies that allows the application to load quickly on all types of devices (computers, tablets and phones). This application allows to build and preserve the history of communities or groups. It makes the group dynamic: members can connect to their account on the community, read announcements, consult the balance of their account, add credit, see the transactions of their account, etc....",
+
+                "counts.section.communities":"Communities",
+                "counts.section.happy_members":"Happy Members",
+                "counts.section.transactions": "Transactions Per Day",
+                "counts.section.plans": "Plans",
+
+                "services.section.services": "Services",
+                "services.section.check_our": "Check our",
+                "services.section.komunityoo": "Komunityoo offers you a wide range of features to allow you to better manage your community",
+                "services.section.financial_report": "Financial reports",
+                "services.section.you_can": "You can view your community's detailed financial reports in real time",
+                "services.section.member_access": "Member access",
+                "services.section.configure": "Configure your members' access: everyone only uses what he is allowed to use",
+                "services.section.family": "Families Management",
+                "services.section.group": "Group your members together as a family to facilitate their management.",
+                "services.section.cash": "Cash Flow",
+                "services.section.manage": "Manage all of your community's accounts and financial transactions",
+                "services.section.executive": "Executive office",
+                "services.section.setup": "Set up an executive office to assist you in managing the community",
+                "services.section.community_events": "Community events",
+                "services.section.easily": "Easily organize events in your community and follow their evolution over time",
+
+                "pricing.section.pricing": "Pricing",
+                "pricing.section.discover":"Discover all our subscription offers to select the perfect plan for you and your community",
+                "pricing.section.free": "Free",
+                "pricing.section.month": "/ month",
+                "pricing.section.buy_now": "Buy Now",
+                "pricing.section.business": "Business",
+                "pricing.section.developer": "Developer",
+                "pricing.section.advanced": "Advanced",
+                "pricing.section.ultimate": "Ultimate",
+                
+                "contact.section.contact": "Contact",
+                "contact.section.contact_us": "Contact Us",
+                "contact.section.message": "A message from you will always please us. Contact us and we will answer you as soon as possible",
+                "contact.section.adress": "Our Address",
+                "contact.section.email_us": "Email Us",
+                "contact.section.call_us": "Call Us",
+                "contact.section.your_message": "Your message has been sent. Thank you!",
+                "contact.section.send_message": "Send Message",
+                "contact.section.loading": "Loading",
+
+                "footer.join": "Join Our Newsletter",
+                "footer.be_always":"Be always informed of our news by subscribing to our newsletter",
+                "footer.phone": "Phone:",
+                "footer.email": "Email:",
+                "footer.home":"Home",
+                "footer.services": "Services",
+                "footer.terms":"Terms of service",
+                "footer.privacy":"Privacy policy",
+                "footer.about_us":"About us",
+                "footer.Our_history": "Our history",
+                "footer.ecosystem": "The ecosystem",
+                "footer.parteners":"Partners",
+                "footer.social_network":"Our Social Networks",
+                "footer.stay": "Stay close to us through all our different accounts on social networks so that you don't miss anything in our evolution",
+                "footer.all_right": "All Rights Reserved",
+                "footer.suscribe": "Suscribe"
+            }
+        },
+        "Fr": {
+            "strings": {
+                "navMenu.home":"Accueil",
+                "navMenu.about": "A propos",
+                "navMenu.services": "Services",
+                "navMenu.team": "Equipe",
+                "navMenu.showcase": "Vitrine",
+                "navMenu.pricing": "Tarif",
+                "navMenu.testimonials": "Témoignages",
+                "navMenu.contact": "Contact",
+                
+                "header.welcome": "We build helpful software",
+                "header.welcome.next": "For Business and Startups",
+                "header.manage_community": "Gérez votre communauté ou votre groupe en toute simplicité!",   
+                "header.get_started": "Démarrer",
+                "header.watch_video": "Regarder la vidéo",
+                "header.komunityoo": "Komunityoo",
+
+                "service.section.highly_configurable": "Hautement configurable",
+                "service.section.customize": "Personnalisez l'utilisation de la communauté par les membres en fonction de votre politique.",
+                "service.section.report": "Rapport détaillé",
+                "service.section.get_log": "Obtenez un journal de l'activité de votre communauté à tout moment sur une période donnée.",
+                "service.section.highly_performance": "Haute performance",
+                "service.section.every_operation": "Chaque opération est effectuée rapidement et avec précision pour une expérience utilisateur optimale.",
+                "service.section.connectivity": "Connectivité",
+                "service.section.stay_close" : "Restez proche de votre communauté, où que vous soyez.",
+
+                "about.section.about": "À propos",
+                "about.section.find": "Pour en savoir plus",
+                "about.section.about_us":"A propos de nous",
+                "about.section.find_out": "Découvrez qui nous sommes, nos objectifs et notre vision du rapprochement des peuples.",
+                "about.section.secure": "Une application web sécurisée qui permet de gérer facilement et simplement une communauté ou un groupe.",
+                "about.section.komunityoo": "Komunityoo est une application web sécurisée qui permet de gérer facilement et simplement une communauté ou un groupe.Elle est développée en utilisant des technologies récentes qui permettent à l'application de se charger rapidement sur tous les types d'appareils (ordinateurs, tablettes et téléphones). Cette application permet de construire et de conserver l'historique des communautés ou des groupes. Elle rend le groupe dynamique : les membres peuvent se connecter à leur compte sur la communauté, lire les annonces, consulter le solde de leur compte, ajouter du crédit, voir les transactions de leur compte, etc....",
+
+                "counts.section.communities":"Communautés",
+                "counts.section.happy_members":"Membres heureux",
+                "counts.section.transactions": "Transactions par jour",
+                "counts.section.plans": "Offres",
+
+                "services.section.services": "Services",
+                "services.section.check_our": "Consultez notre",
+                "services.section.komunityoo": "Komunityoo vous offre un large éventail de fonctionnalités pour vous permettre de mieux gérer votre communauté",
+                "services.section.financial_report": "Rapports financiers",
+                "services.section.you_can": "Vous pouvez consulter les rapports financiers détaillés de votre communauté en temps réel.",
+                "services.section.member_access": "Accès pour les membres",
+                "services.section.configure": "Configurez l'accès de vos membres : chacun n'utilise que ce qu'il est autorisé à utiliser",
+                "services.section.family": "Gestion des familles",
+                "services.section.group": "Regroupez vos membres en tant que famille pour faciliter leur gestion.",
+                "services.section.cash": "Cash Flow",
+                "services.section.manage": "Gérer tous les comptes et toutes les transactions financières de votre communauté",
+                "services.section.executive": "Bureau exécutif",
+                "services.section.setup": "Mettre en place un bureau exécutif pour vous aider à gérer la communauté.",
+                "services.section.community_events": "Événements communautaires",
+                "services.section.easily": "Organisez facilement des événements dans votre communauté et suivez leur évolution dans le temps.",
+            
+
+                "pricing.section.pricing": "Tarification",
+                "pricing.section.discover":"Découvrez toutes nos offres d'abonnement pour choisir la formule idéale pour vous et votre communauté.",
+                "pricing.section.free": "Gratuit",
+                "pricing.section.month": "/ mois",
+                "pricing.section.buy_now": "Acheter maintenant",
+                "pricing.section.business": "Business",
+                "pricing.section.developer": "Développeur",
+                "pricing.section.advanced": "Avancé",
+                "pricing.section.ultimate": "Ultimate",
+
+                "contact.section.contact": "Contact",
+                "contact.section.contact_us": "Contactez-nous",
+                "contact.section.message": "Un message de votre part nous fera toujours plaisir. Contactez-nous et nous vous répondrons dans les plus brefs délais.",
+                "contact.section.adress": "Notre adresse",
+                "contact.section.email_us": "Envoyez-nous un courriel",
+                "contact.section.call_us": "Appelez-nous",
+                "contact.section.your_message": "Votre message a été envoyé. Merci !",
+                "contact.section.send_message": "Envoyer un message",
+                "contact.section.loading": "Chargement",
+
+                "footer.join": "Inscrivez-vous à notre newsletter",
+                "footer.be_always":"Soyez toujours informé de nos actualités en vous inscrivant à notre newsletter",
+                "footer.phone": "Téléphone:",
+                "footer.email": "Courriel :",
+                "footer.home":"Accueil",
+                "footer.services": "Services",
+                "footer.terms":"Conditions d'utilisation",
+                "footer.privacy":"Politique de confidentialité",
+                "footer.about_us":"A propos de nous",
+                "footer.Our_history": "Notre histoire",
+                "footer.ecosystem": "L'écosystème",
+                "footer.parteners":"Partenaires",
+                "footer.social_network":"Nos réseaux sociaux",
+                "footer.stay": "Restez proche de nous grâce à nos différents comptes sur les réseaux sociaux afin de ne rien manquer de notre évolution.",
+                "footer.all_right": "Tous droits réservés",
+                "footer.suscribe": "S'inscrire"
+        
+            }
+        }
+        
+}
+
+function changeLanguage(){
+    var x = document.getElementById("selectedLang");
+    
+    let currentLangue= "";
+    if(x.value == "French"){
+      currentLangue="Fr";
+      
+    }
+    if(x.value == "English"){
+      currentLangue="En";
+      
+    }
+    var lang = document.getElementById("myBody");
+    lang.setAttribute("lang", currentLangue);
+
+  }
+
+document.addEventListener('change', () => {
+    let zones = document.querySelectorAll('html [lang]');
+    applyStrings(zones);
+
+    let lang = findLocaleMatch();
+    let container = document.querySelector(`html [lang*=${lang}]`);
+    container.className = 'lang-match';
+});
+
+function applyStrings(containers) {
+    containers.forEach(container => {
+        let locale = container.getAttribute('lang');
+        container.querySelectorAll(`[data-key]`).forEach(element => {
+            let key = element.getAttribute('data-key');
+            let lang = locale.substr(0, 2); 
+            if (key) {
+                element.textContent = langdata[lang].strings[key];
+            }
+        });
+    })
+}
+
+function findLocaleMatch() {
+    let keys = Object.keys(langdata);
+    let locales = Intl.getCanonicalLocales(keys); 
+
+    let lang = navigator.language; 
+    let locale = Intl.getCanonicalLocales(lang);
+    console.log('browser language', lang);
+    console.log('locales from data file', locale);
+
+    let langMatch = document.documentElement.getAttribute('lang'); 
+    locales = locales.filter(l => locale == l);
+    langMatch = (locales.length > 0) ? locales[0] : langMatch;
+    return langMatch;
+}
